1646aa9431ac6410e6eb313275a77f211fc916fc
Will <Willi@BANDBOOK>
2024-11-07 10:04:01 -0500

    Initialized Visual Studio Project

---
+++ Lab Problem 9.3/9.3.cpp
@@ -0,0 +1,17 @@
+// File Name:     Lab Problem 9.3
+// Author:        William Inkrott
+// Date:          11/07/24
+// Description:   Check 20 dice rolls for runs. 
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void displayRun(int values[], int size);
+
+int main(void)
+{
