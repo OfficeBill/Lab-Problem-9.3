42c998fba6e4affe688bbc73b58b7a38cef63065
Will <Willi@BANDBOOK>
2024-11-07 10:06:53 -0500

    Random dice functioning

--- Lab Problem 9.3/9.3.cpp
+++ Lab Problem 9.3/9.3.cpp
@@ -15,3 +15,50 @@ void displayRun(int values[], int size);
 
 int main(void)
 {
+	int values[], size = 20;
+	srand(time(0));
+	for (int i = 0; i < 20; i++)
+	{
+		int max_value = 6;
+		int randnum = rand() % max_value;
+		switch (randnum)
+		{
+		case 0:
+			values[i] = 1;
+			break;
+		case 1:
+			values[i] = 2;
+			break;
+		case 2:
+			values[i] = 3;
+			break;
+		case 3:
+			values[i] = 4;
+			break;
+		case 4:
+			values[i] = 5;
+			break;
+		case 5:
+			values[i] = 6;
+			break;
+		}
+	}
+}
+
+void displayRun(int values[], int size)
+{
+	int runnum = 1
+		bool run;
+	for (int i = 0; i < size; i++)
+	{
+		if (values[i] == values[i + 1])
+		{
+			runnum = 1;
+			run = true;
+			for (run; run != true; runnum + 1)
+			{
+
+			}
+		}
+	}
+}
