e939caccab1ce231041246d00c4b690a21141b5b
Will <Willi@BANDBOOK>
2024-11-08 11:26:13 -0500

    completed displayRun

--- Lab Problem 9.3/9.3.cpp
+++ Lab Problem 9.3/9.3.cpp
@@ -20,45 +20,33 @@ int main(void)
 	for (int i = 0; i < 20; i++)
 	{
 		int max_value = 6;
-		int randnum = rand() % max_value;
-		switch (randnum)
-		{
-		case 0:
-			values[i] = 1;
-			break;
-		case 1:
-			values[i] = 2;
-			break;
-		case 2:
-			values[i] = 3;
-			break;
-		case 3:
-			values[i] = 4;
-			break;
-		case 4:
-			values[i] = 5;
-			break;
-		case 5:
-			values[i] = 6;
-			break;
-		}
+		int randnum = rand() % max_value+1;
+		values[i] = randnum;
+		cout << values[i] << " ";
 	}
-	displayRun;
+	cout << endl;
+	displayRun(values, size);
 
 }
 
 void displayRun(int values[], int size)
 {
-		for (int i = 0; i < 20; i++)
+		for (int i = 0; i < size; i++)
 	{
 		if (values[i] == values[i + 1])
 		{
-			cout << "(";
+			cout << "(" << values[i] << " " << values[i + 1];
+			while (values[i] == values[i + 2])
+			{
+				cout << " " << values[i];
+				i++;
+			}
+			cout << ") ";
+			i = i + 1;
 		}
-		cout << values[i];
-		if (values[i] == values[i - 1])
+		else
 		{
-			cout << ")";
+			cout << values[i] << " ";
 		}
 	}
 }
